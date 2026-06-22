985edf8bb26ce0d90e25feb5fbef3ac22c98010a
orojansandor-prog <orojansandor@gmail.com>
2026-06-22 21:26:21 +0200

    SEO: noscript static content for crawlers, remove react-snap (main.tsx)

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,16 @@
 import { StrictMode } from 'react'
-import { createRoot, hydrateRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { AuthProvider } from './lib/auth'
 import { I18nProvider } from './lib/i18n'
 
-const root = document.getElementById('root')!
-const app = (
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <I18nProvider>
       <AuthProvider>
         <App />
       </AuthProvider>
     </I18nProvider>
-  </StrictMode>
+  </StrictMode>,
 )
-
-// react-snap prerendered pages use hydrateRoot, normal SPA uses createRoot
-if (root.hasChildNodes()) {
-  hydrateRoot(root, app)
-} else {
-  createRoot(root).render(app)
-}
